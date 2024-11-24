f3bc2f590d020d45f6dc22881c91b60650f9c073
Fred <6550969+MagicFred888@users.noreply.github.com>
2024-11-24 20:16:51 +0100

    Add project files.

---
+++ AdventOfCode2024/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Blocker Code Smell", "S2368:Public methods should not have multidimensional array parameters", Justification = "Part of a toolbox, maybe used later", Scope = "member", Target = "~M:AdventOfCode2024.Tools.Tools.DebugPrint(System.Object[,],System.Collections.Generic.Dictionary{System.String,System.String},System.String)")]
+[assembly: SuppressMessage("Blocker Code Smell", "S2368:Public methods should not have multidimensional array parameters", Justification = "Part of a toolbox, maybe used later", Scope = "member", Target = "~M:AdventOfCode2024.Tools.Tools.DebugPrint(System.Char[,])")]
+[assembly: SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "Part of a toolbox, maybe used later", Scope = "member", Target = "~M:AdventOfCode2024.Tools.Tools.LCM(System.Int64[])~System.Int64")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Part of a toolbox, maybe used later", Scope = "member", Target = "~M:AdventOfCode2024.Tools.Tools.LCM(System.Int64[])~System.Int64")]
---
+++ AdventOfCode2024/Solver/Day01.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2024.Solver
+{
+    internal class Day01 : BaseSolver
+    {
+        public override string PuzzleTitle { get; } = "Add challenge name here...";
+
+        public Day01(int day) : base(day)
+        {
+        }
+
+        public override string GetSolution1()
+        {
+            // Case 1
+            return "Not implemented !";
+        }
+
+        public override string GetSolution2()
+        {
+            // Case 2
+            return "Not implemented !";
+        }
+    }
+}
